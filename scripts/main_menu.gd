65c8b65b0f6030feb2e8db43c2de4d18f785e7a8
Paul Peng <paulpeng@gmail.com>
2025-07-16 10:37:58 -0400

    Improved main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-var playing = false
+signal play
+var playing := false
 
 func _input(event):
 	if event is InputEventMouseMotion and playing == false:
@@ -10,14 +11,22 @@ func _input(event):
 		%Camera3D.rotation_degrees.z = clamp(%Camera3D.rotation_degrees.z, -4.0, 4.0)
 
 func _process(delta: float) -> void:
-	if !%BGSound.playing:
-		%BGSound.play()
+	if playing == false:
+		if !%BGSound.playing:
+			%BGSound.play()
+
+		var rand_addition := randf()
+		if(rand_addition >= 0.975):
+			%Blackjack.visible = false
+			await get_tree().create_timer(0.5).timeout
+			if(playing == false):
+				%Blackjack.visible = true
+	
 		
-func _on_play_pressed() -> void:
+func _on_start() -> void:
 	playing = true
-	%"Main Menu UI".visible = false
 	%Blackjack.visible = false
-	%SpotLight3D.light_energy = 2.0
+	%MainMenuUI.visible = false
 	
 	var x_pos_diff = abs(0.431 - %Camera3D.position.x)
 	var y_pos_diff = abs(1.192 - %Camera3D.position.y)
@@ -32,3 +41,12 @@ func _on_play_pressed() -> void:
 		%Camera3D.rotation_degrees.x += x_rot_diff / 100
 		%Camera3D.rotation_degrees.y += y_rot_diff / 100
 		await get_tree().create_timer(0.01).timeout
+	
+	play.emit()
+
+func _play() -> void:
+	%SpotLight3D.light_energy = 2.0
+	%BGSound.stop()
+	%ThumpSound.play()
+	await %ThumpSound.finished
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
--- scripts/main_menu_ui.gd
+++ scripts/main_menu_ui.gd
@@ -1,4 +1,35 @@
 extends Control
 
+signal start
+@onready var hover_sound = get_node("HoverSound")
+@onready var click_sound = get_node("ClickSound")
+var alpha := 0.5
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		%BeepBeep.play()
+		start.emit()
+
+func _process(delta: float) -> void:
+	var rand_addition = (randf() - 0.5) / 10.0
+	alpha += rand_addition
+	alpha = clamp(alpha, 0.0, 1.0)
+	%PressAnyKey.modulate = Color(1.0, 1.0, 1.0, alpha)
+
+func _on_play_pressed() -> void:
+	click_sound.play()
+
+func _on_play_mouse_entered() -> void:
+	hover_sound.play()
+
+func _on_shop_pressed() -> void:
+	click_sound.play()
+
+func _on_shop_mouse_entered() -> void:
+	hover_sound.play()
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_quit_mouse_entered() -> void:
+	hover_sound.play()
